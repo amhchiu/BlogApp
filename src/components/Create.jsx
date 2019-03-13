053bbc9c2ecd5c6d71a26bf5b9c42cefc3067852
amhchiu <physicspianist@gmail.com>
2019-03-13 22:53:55 +0000

    starting slatejs editor on create page

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -3,22 +3,49 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 
 import { Editor } from 'slate-react';
+import { Value } from 'slate';
 
 const styles = theme => ({
-    
+
+});
+
+const initialValue = Value.fromJSON({
+    document: {
+        nodes: [
+            {
+                object: 'block',
+                type: 'paragraph',
+                nodes: [
+                    {
+                        object: 'text',
+                        leaves: [
+                            {
+                                text: 'A line of text in a paragraph.',
+                            },
+                        ],
+                    },
+                ],
+            },
+        ],
+    },
 });
 
 class Create extends React.Component{
 
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
+        this.state = {
+            value: initialValue 
+        };
+    }
+    
+    handleChange(e){
+        this.setState({value: e.value});
     }
 
     render(){
         return(
-            <div>
-                hi
-            </div>
+            <Editor value={this.state.value} onChange={this.handleChange.bind(this)} />
         );
     }
 }
@@ -26,5 +53,5 @@ class Create extends React.Component{
 export default withStyles(styles)(Create);
 
 Create.propTypes = {
-    
+
 };
